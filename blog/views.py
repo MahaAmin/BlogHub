6513f2f911e4af8207b5ba2aa785725bb8694332
MahaAmin <mahaamin97@gmail.com>
2020-03-23 19:57:21 +0200

    Add BlogHub About view

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>BlogHub Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>BlogHub About</h1>')
