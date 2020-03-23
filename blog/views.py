7fa931c45f46c2fa1f35a048993d9d9dba35a44b
MahaAmin <mahaamin97@gmail.com>
2020-03-23 22:15:40 +0200

    Add context by dummy data in views, and block-code in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': "Maha Amin",
+        'title': "Blog Post 1",
+        'content': 'First post content.',
+        'date_posted': 'March 20 2020'
+    },
+    {
+        'author': "Nayera Mohamed",
+        'title': "Blog Post 2",
+        'content': 'Second post content.',
+        'date_posted': 'March 22 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
