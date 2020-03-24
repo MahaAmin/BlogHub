b2fc5670636d44a43d0c536ff1769b4475350d8e
MahaAmin <mahaamin97@gmail.com>
2020-03-24 11:55:37 +0200

    Use Post model in views instead of dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': "Maha Amin",
-        'title': "Blog Post 1",
-        'content': 'First post content.',
-        'date_posted': 'March 20 2020'
-    },
-    {
-        'author': "Nayera Mohamed",
-        'title': "Blog Post 2",
-        'content': 'Second post content.',
-        'date_posted': 'March 22 2020'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
